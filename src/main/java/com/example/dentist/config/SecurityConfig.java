3b7b0eb7e1b4fbebc1c4ab1cbc4f76c4c5b62413
Kamil Chaciak <kamil.chaciak@omegacode.pl>
2018-01-10 21:39:55 +0100

    security

---
+++ src/main/java/com/example/dentist/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.dentist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Profile(ProfileNames.DATABASE)
+    public PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests().antMatchers("/statics/**", "/webjars/**", "/", "/registrationForm.html", "/error.html").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
+
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+}
--- src/main/java/com/example/dentist/config/WebMvcConfig.java
+++ src/main/java/com/example/dentist/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.dentist;
+package com.example.dentist.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
